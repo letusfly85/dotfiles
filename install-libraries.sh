6041be4be19acb8c4716bfa1ac3225302e026bfe
letusfly85 <letusfly95@gmail.com>
2021-05-22 11:57:14 +0900

    :package: add AWS eksctl

--- install-libraries.sh
+++ install-libraries.sh
@@ -1,13 +1,18 @@
 #/usr/bin/env bash
 
+# Terminal
 brew install starship
 
 
+# Tail Managers
 # https://github.com/koekeishiya/dotfiles/blob/master/yabai/yabairc
 brew install koekeishiya/formulae/yabai
 brew install koekeishiya/formulae/skhd
-
 brew services start skhd
 brew services start yabai
 
+# Terminal
 brew cask install alacritty
+
+# AWS EKS
+brew install weaveworks/tap/eksctl
