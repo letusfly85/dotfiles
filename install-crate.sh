c75f677e8e9da834f3f19171f219700958bfb3ae
letusfly85 <shunsuke.wada@wonder-soft.com>
2023-07-28 21:54:27 +0900

    Remove unused crate installations and update dependencies

--- install-crate.sh
+++ install-crate.sh
@@ -4,16 +4,6 @@ cargo install \
     exa \
     bat \
     jaq \
-    pueue \
-    gitui \
     atuin \
-    git-delta \
     just \
-    cargo-make \
-    cargo-hf2 \
-    hf2-cli \
     cargo-generate
-
-cargo install diesel_cli --no-default-features --features mysql
-
-cargo install --git https://github.com/XAMPPRocky/tokei.git tokei
